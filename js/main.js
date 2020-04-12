a3d81df5b258ba7b368f5ee1eb71ae37a00c821d
Gerasimov Ruslan <gerassimov.ruslan@gmail.coom>
2020-04-12 13:05:11 +0300

    code from index.js to main.js

--- js/main.js
+++ js/main.js
@@ -57,3 +57,10 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
     })
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const buttonsContainer = document.querySelector('.buttons-wrap');
+
+    buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
+        '<button class="button">Нет</button>';
+});
